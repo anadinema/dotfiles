00a77a609f338e1f3649d1469a594e699c24c181
Anadi Nema <anadi.nema@outlook.com>
2024-03-16 13:19:43 +0100

    bot : changes in following dotfiles
    M  brew/Brewfile
    A  settings/raycast/night-shift.sh
    M  settings/raycast/settings.rayconfig
    A  settings/raycast/settings.rayconfig.bkp-20240316-13:19:43
    M  source/.functions

---
+++ settings/raycast/night-shift.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Night shift
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ☀️
+# @raycast.packageName Productivity, Utility
+
+# Documentation:
+# @raycast.description Toggle the night shift on or off
+# @raycast.author Anadi Nema
+# @raycast.authorURL https://github.com/anadinema
+
+shortcuts run night-shift
+
+echo "Night shift toggled ☀️"
